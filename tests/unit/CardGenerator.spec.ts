889e9f1655cd38bb8bf3c3a23daa17d7d36a0673
kymiller <kylemiller457@gmail.com>
2025-03-05 21:05:02 -0600

    Adding jest to the project

---
+++ tests/unit/CardGenerator.spec.ts
@@ -0,0 +1,9 @@
+import test, { describe } from "node:test";
+import CardGenerator from "src/CardGenerator";
+
+
+describe("Generator", () => {
+    test("Create a generator instance", () => {
+        const generator = new CardGenerator();
+    })
+})
