824b065fcbe6ba30fff632f4a2a95cc31d69e5a2
kymiller <kylemiller457@gmail.com>
2025-03-05 20:27:04 -0600

    init commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+
+import { Plugin } from "obsidian"
+
+
+export default class FCGPlugin extends Plugin {
+
+    async onload(): Promise<void> {
+        console.log("Todo build the application!")
+    }
+
+    async onunload(): Promise<void> {
+        console.log("Todo build the application!")
+    }
+
+}
