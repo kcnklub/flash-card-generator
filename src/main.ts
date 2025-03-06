b0b88a443036feb3c454ad0c4542e8c44eef0e2d
kymiller <kylemiller457@gmail.com>
2025-03-05 20:41:51 -0600

    Adding CardGenerator as a way to generator the cards given a Markdown file.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 
 import { Plugin } from "obsidian"
-
+import CardGenerator from "./CardGenerator"
 
 export default class FCGPlugin extends Plugin {
 
+
+    private cardGenerator: CardGenerator;
+
+
     async onload(): Promise<void> {
-        console.log("Todo build the application!")
+        console.log("Plugin Loading")
+        this.cardGenerator = new CardGenerator();
     }
 
     async onunload(): Promise<void> {
